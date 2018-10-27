0aa8710fe2948a880b3ef0cfe04e9e2eb22afbbc
nikita494 <44530883+nikita494@users.noreply.github.com>
2018-10-27 20:58:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1{
+    class Program{
+ public static void Main(string[] args){
+            string input;
+            input = Console.ReadLine();
+List<int> indexes = StringWorker.FindAllIndexes(" ",input) ;
+                indexes.Insert(0, 0);
+               if (input.IndexOf(" ", StringComparison.OrdinalIgnoreCase) != -1) 
+             Console.WriteLine("Индекс(ы) пробела(ов) ");
+            indexes.ForEach(Console.WriteLine);
+            Console.WriteLine(StringWorker.ReverseWordsInString(input));
+            Console.ReadLine();
+           }}}
---
+++ StringWorker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class StringWorker
+    {
+
+
+        public static List<int> FindAllIndexes(string a, string z)
+        {
+            int x = 0, i = z.IndexOf(a, x, StringComparison.OrdinalIgnoreCase);
+            List<int> indexes = new List<int>();
+            while (i != -1)
+            {
+                indexes.Add(i);
+                x = i + 1;
+                i = z.IndexOf(a, x, StringComparison.OrdinalIgnoreCase);
+            }
+            return indexes;
+
+        }
+        public static string ReverseWordsInString(string input)
+        {
+            string[] answerArray = Regex.Split(input, @"(\s+)", RegexOptions.ExplicitCapture);
+            Array.Reverse(answerArray);
+             return String.Join(" ", answerArray);
+
+        }
+        
+
+    }
+}
